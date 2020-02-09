d33a48c40af64a68830b727f2cd360de1b542cd9
sommersoft <sommersoft@gmail.com>
2020-02-08 21:32:57 -0600

    WIP: add packaging/installation

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='physaci_subscriber',
+    version='0.0.1',
+    packages=find_packages('physaci_subscriber'),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'physaci_send_subscription = physaci_subscriber.subscribe:subscribe_to_registrar'
+        ]
+    },
+)
