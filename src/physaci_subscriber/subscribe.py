de313271b38a0cf1bcfe0a508fc0fcfee9ba1ab6
sommersoft <sommersoft@gmail.com>
2020-02-10 20:20:36 -0600

    subscribe.py: clean up and re-direct some of the logging messages

--- src/physaci_subscriber/subscribe.py
+++ src/physaci_subscriber/subscribe.py
@@ -59,10 +59,13 @@ class PhysaCISubscribe():
         if secrets.compare_digest(old_key, new_key):
             self.cofiguration.node_sig_key = ""
             self.configuration.write_config()
-            raise RuntimeError(
+            conf_loc = self.configuration.config_location
+            message = (
                 "Could not generate acceptable key. "
-                "'{}' updated with null key value.".format(self.configuration.config_location)
+                "'{}' updated with null key value.".format(config_loc)
             )
+            physaci_logger.error(message)
+            raise RuntimeError(message)
 
         self.configuration.node_sig_key = new_key
 
@@ -101,6 +104,10 @@ class PhysaCISubscribe():
             if response.ok:
                 busy_status = response.json()
         except requests.exceptions.ConnectionError:
+            physaci_logger.warning(
+                'Could not determine node busy status. Connection to node '
+                'server failed.'
+            )
             pass
 
         return busy_status
@@ -110,21 +117,21 @@ class PhysaCISubscribe():
             a new node signature key for use with an HTTP Signature header
             inside pushed notifications.
         """
-        physaci_logger.info('Initiating physaCI registrar subscription...')
+        physaci_logger.info('Initiating physaCI registrar subscription.')
         sub_message = {
             'node_name': gethostname(),
             'listen_port': self.configuration.listen_port,
             'busy': self.node_busy_status().get('busy', False)
         }
 
-        physaci_logger.info('Generating new node signature key...')
+        debug_logger.info('Generating new node signature key.')
         self.generate_node_key()
         sub_message['node_sig_key'] = self.configuration.node_sig_key
 
         url = self.configuration.physaci_registrar_url
         header = {'x-functions-key': self.configuration.physaci_api_key}
 
-        physaci_logger.info('Sending subscription request...')
+        debug_logger.info('Sending subscription request.')
         response = requests.post(url, headers=header, json=sub_message)
         if response.ok:
             self.configuration.write_config()
